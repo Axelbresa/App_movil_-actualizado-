9bec1b1a0226f84f248f4894b2744f0409e871e3
AxelBresa <bresanovichaxel43@gmail.com>
2024-05-27 11:22:05 -0300

    Se le agrego una bienvenida al inicio

---
+++ app/Register.tsx
@@ -0,0 +1,131 @@
+import React from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import {usuarios} from "./panel/lista_usuarios"
+import { useNavigation } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons'; 
+import CustomButton from './panel/customButton';
+
+function Register() {
+  const navigation = useNavigation();
+  const [username, setUsername] = React.useState('');
+  const [correo, setCorreo] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [confirmarPassword, setConfirmarPassword] = React.useState('');
+
+  const handleLogin = () => {
+
+    const correoValido = correo.includes('@');
+    const usuarioEncontrado = usuarios.find(
+        user => user.nombre === username  && user.correo === correo && user.contraseña === password
+      );
+  
+      if (usuarioEncontrado) {
+            alert('Usuario logeado');
+            navigation.navigate('Logeado', { username: usuarioEncontrado.nombre });
+      } else {
+        if (!correoValido) {
+            alert("Debe incluir '@' en el correo electrónico");
+        } if (password.length<8) {
+            alert('La contraseña debe ser como minimo de 8 caracteres');
+        } if (username=="" || correo=="" || password=="") {
+            alert('No debe estar vacio ningun campo');
+        } 
+
+      }
+  };
+
+  return (
+    <View style={styles.container}>
+    <Text style={styles.title}>Login:</Text>
+    <Text style={styles.label}>Nombre de usuario:</Text>
+      <View style={styles.inputContainer}>
+        <FontAwesome name="user" size={24} color="black" style={styles.icon} />
+        <TextInput
+          style={styles.input}
+          value={username}
+          onChangeText={setUsername}
+          placeholder="Nombre de usuario"
+        />
+      </View>
+
+    <Text style={styles.label}>Correo:</Text>
+      <View style={styles.inputContainer}>
+        <FontAwesome name="envelope" size={24} color="black" style={styles.icon} />
+        <TextInput
+          style={styles.input}
+          value={correo}
+          onChangeText={setCorreo}
+          placeholder="Correo electrónico"
+        />
+      </View>
+
+    <Text style={styles.label}>Password:</Text>
+      <View style={styles.inputContainer}>
+        <FontAwesome name="lock" size={24} color="black" style={styles.icon} />
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={true}
+          placeholder="Contraseña"
+        />
+      </View>
+      <Text style={styles.label}>Confirmar password:</Text>
+      <View style={styles.inputContainer}>
+        <FontAwesome name="lock" size={24} color="black" style={styles.icon} />
+        <TextInput
+          style={styles.input}
+          value={confirmarPassword}
+          onChangeText={setConfirmarPassword}
+          secureTextEntry={true}
+          placeholder="Contraseña"
+        />
+      </View>
+        <CustomButton title="Iniciar sesión" onPress={handleLogin} />
+      <View style={styles.franja}></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: 'purple', // Fondo claro para el contenedor
+  },
+  title: {
+    color:"white",
+    fontSize:40,
+    marginBottom:20
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+    backgroundColor: '#f0f0f0', // Fondo claro para los campos de entrada
+    borderRadius: 50, // Bordes redondeados
+  },  
+  franja:{
+    height: "40%",
+    width: "100%",
+    backgroundColor: "white",
+    // position: "absolute",
+    marginTop:2,
+    // borderTopEndRadius: 50,
+  },
+  icon: {
+    marginHorizontal: 10,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    paddingHorizontal: 10,
+  },
+  label:{
+    color:"white",
+  },
+});
+
+export default Register;
--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,82 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import LoginScreen from './Login';
-import LogeadoScreen from './Logeado';
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
+function Index() {
+  const navigation = useNavigation();
 
-function index() {
   return (
-    <View>
-        <TouchableOpacity>Login</TouchableOpacity>
-        <TouchableOpacity>Register</TouchableOpacity>
+    <View style={styles.container}>
+
+      <View style={styles.imageContainer}>
+        <Image
+          source={{ uri: '../assets/images/Logo.jpg' }} 
+          style={styles.image}
+        />
+      </View>
+
+      <Text style={styles.welcomeText}>Bienvenido a la App de Tareas</Text>
+      <Text style={styles.descriptionText}>
+        Regístrate para poder crear tus tareas y demás.
+      </Text>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Register')}>
+          <Text style={styles.buttonText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
- 
   );
 }
 
-export default index;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'purple',
+  },
+  imageContainer: {
+    // marginBottom: 20, // Espacio entre la imagen y los botones
+
+  },
+  image: {
+    width: 250,
+    height: 250,
+  },
+  welcomeText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  descriptionText: {
+    fontSize: 16,
+    color: 'gray',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    width:"100%",
+    height:"10%",
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    marginHorizontal: 5, // Para la separación de 10px total (5px de cada lado)
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
+
+export default Index;
